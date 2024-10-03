98231d6e44c74ae575c182f6a3bc9fb15304c545
王佳琪 <you@example.com>
2024-10-03 14:18:42 +0930

    save

---
+++ Object.cpp
@@ -0,0 +1,35 @@
+#include"Object.h"
+#include<vector>
+
+    void Object:: update(float deltaTime){
+
+    };// Updates the object’s state based on time elapsed (deltaTime).
+    void Object::render();//Draws the object on the screen.
+    bool Object::checkCollision(const Object& other) const{
+
+    };// Checks if this object is colliding with another object (other).
+    void Object::onCollision(Object& other);//Handles the logic when a collision with another object occurs.
+//Getters
+    Vector2D Object::getPosition() const{
+        return position;
+    };// Returns the object’s current position.
+    Vector2D Object::getVelocity() const{
+        return velocity;
+    };// Returns the object’s current velocity.
+    float Object::getRotation() const{
+        return rotation;
+    }; //Returns the object’s current rotation angle.
+    bool Object::isActive() const{
+        return active;
+    };// Returns whether the object is active.
+//Setters
+    void Object::setPosition(const Vector2D& newPosition){
+        position=newPosition;
+    };//Sets the object’s position to newPosition.
+    void Object::setVelocity(const Vector2D& newVelocity){
+        velocity=newVelocity;
+    };// Sets the object’s velocity to newVelocity.
+    void Object::setRotation(float newRotation){
+        rotation=newRotation;
+    };// Sets the object’s rotation to newRotation.
+    void Object::setActive(bool isActive);// Sets the object’s active status.
---
+++ Player.h
@@ -0,0 +1,8 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include"Object.h"
+
+class Player:public : Object{
+
+};
+#endif
