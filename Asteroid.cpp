aceb8c2c91ef36ff12645def1460affd791348e1
AlexCLynch <alex@Ligma>
2024-10-10 14:30:35 +1030

    added asteroid outline

--- Asteroid.cpp
+++ Asteroid.cpp
@@ -6,6 +6,8 @@
 Asteroid::Asteroid(int size): Object(new sf::CircleShape(10 + size*size*3, 7)){
     sf::Shape* shape = getShape();
     shape->setOrigin(10 + size*size*3, 10 + size*size*3);
-    (*shape).setFillColor(sf::Color(150, 50, 250));
+    (*shape).setFillColor(sf::Color(50, 50, 50));
+    shape->setOutlineThickness(1.f);
+    shape->setOutlineColor(sf::Color(255, 255, 255));
     this->size = size;
 };
