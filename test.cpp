82d01ec5b89aceb282c50c02e369a6101507cd3f
王佳琪 <you@example.com>
2024-10-14 22:14:44 +1030

    test

--- test.cpp
+++ test.cpp
@@ -2,6 +2,43 @@
 #include <SFML/Graphics.hpp>
 #include "EasyEnemy.h"
 #include "MediumEnemy.h"
+#include <iostream>
+#include "Projectile.h"
+
+int main() {
+    // create a projectile
+    sf::RectangleShape* projectileShape = new sf::RectangleShape(sf::Vector2f(5.f, 10.f));
+    projectileShape->setFillColor(sf::Color::Yellow);
+
+    // initial position direction spped
+    sf::Vector2f startPosition(400.f, 300.f);
+    sf::Vector2f direction(1.f, 0.f); // move yo right
+    float speed = 200.f;
+
+    // create Projectile 
+    Projectile projectile(projectileShape, speed, startPosition, direction, nullptr);
+
+    // print status
+    std::cout << "Initial Position: (" << projectile.getPosition().x << ", " << projectile.getPosition().y << ")\n";
+    std::cout << "Initial Active Status: " << projectile.isActive() << "\n";
+
+    // update
+    float deltaTime = 1.0f; // 1秒
+    projectile.update(deltaTime);
+
+    // after status
+    std::cout << "After Update Position: (" << projectile.getPosition().x << ", " << projectile.getPosition().y << ")\n";
+    std::cout << "Active Status after Update: " << projectile.isActive() << "\n";
+
+    // bound check
+    projectile.checkOutOfBounds();
+    std::cout << "Active Status after Out-of-Bounds Check: " << projectile.isActive() << "\n";
+
+    // delete
+    delete projectileShape;
+
+    return 0;
+}
 #include "HardEnemy.h"
 #include "Projectile.h"
 
